57f9a3c2f9cf34efe984c59c91cfd6acd0c849bb
Valtteri <valtteri.piironen98@gmail.com>
2026-01-15 10:39:07 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+pisteet = 50  # Muuttujan pisteet arvo on nyt 50
+print(pisteet)  # Tulostaa: 50
+
+pisteet = 20  # Nyt muuttujan pisteet arvo on 120
+print(pisteet)  # Tulostaa: 120
+print ("😂😂")
+
