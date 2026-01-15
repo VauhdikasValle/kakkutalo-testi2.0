ae190797c27df6f46cb27aa6a5c1bf7a37fed944
Valtteri <valtteri.piironen98@gmail.com>
2026-01-15 15:39:55 +0200

    muutoksia

---
+++ Moduuli 2/tehtää 5.py
@@ -0,0 +1,15 @@
+import math
+talents=float(input("Enter talents: "))
+pounds=float(input("Enter pounds: "))
+lots=float(input("Enter lots: "))
+talents_in_grams=20*32*13.3
+pounds_in_grams=32*13.3
+lots_in_grams=13.3
+grams=(talents_in_grams+pounds_in_grams+lots_in_grams)
+total_grams/1000=
+remaining_grams
+
+
+
+
+print ("The weight in modern units: ")
