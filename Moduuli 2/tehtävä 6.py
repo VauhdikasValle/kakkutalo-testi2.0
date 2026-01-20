accf3a7280f0a045fb789506e8f5cfcb3f8bce71
Valtteri <valtteri.piironen98@gmail.com>
2026-01-20 14:23:03 +0200

    muutoksia

--- Moduuli 2/tehtävä 6.py
+++ Moduuli 2/tehtävä 6.py
@@ -3,3 +3,7 @@ import random
 code1 = str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9))
 
 print(code1)
+
+code2 = str(random.randint(1, 6)) + str(random.randint(1, 6)) + str(random.randint(1, 6)) + str(random.randint(1, 6))
+
+print(code2)
