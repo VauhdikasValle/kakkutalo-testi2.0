0ca9103eb714bb8b028287ebc9f14c32655acd7c
Valtteri Piironen <valtteri.piironen98@gmail.com>
2026-01-21 19:01:51 +0200

    muutoksia

--- Moduuli 3/tehtävä 3.py
+++ Moduuli 3/tehtävä 3.py
@@ -1,1 +1,14 @@
-input(Enter biological gender (male or female): ")
+gender = input("Enter biological gender (male or female): ")
+
+if gender == "male":
+    print("male")
+
+elif gender == "female":
+    print("female")
+
+else:
+    print("not a valid gender")
+
+hemogoblin = input("Enter hemoglobin value (g/l): ")
+
+if hemogoblin == "g":
