6aea7318108cb22bdaee484a595d65618891d32b
Valtteri <valtteri.piironen98@gmail.com>
2026-02-09 10:35:09 +0200

    muutoksia22/01

---
+++ Moduuli 8/tehtävä 1.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+yhteys = mysql.connector.connect(
+    host='127.0.0.1',
+    port=3306,
+    database='flight_game',
+    user='Valtteri',
+    password='Pallero1234',
+    autocommit=True
+)
+
